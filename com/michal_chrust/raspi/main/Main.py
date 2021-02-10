a142d5e0daf6800787915af6aa2669b036eb5010
Michał Chrust <michal.chrust@outlook.com>
2021-02-10 19:01:02 +0100

    sample wether app

--- com/michal_chrust/raspi/main/Main.py
+++ com/michal_chrust/raspi/main/Main.py
@@ -1,3 +1,14 @@
 # Main file
+import requests
 
-print('Main file')
+params = {
+    'access_key': '8ec20aa6048dcdc117c84dcfd9cf1ef2',
+    'query': 'Warsaw'
+}
+
+
+params['query'] = 'Paris'
+
+respose = requests.get('http://api.weatherstack.com/current', params)
+j = respose.json()
+print(j['current']['weather_descriptions'])
